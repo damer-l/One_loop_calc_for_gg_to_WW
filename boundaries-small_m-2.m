d7d1a330e1b83b517a2e1a88dcc6c9b0d59b3d8f
Lennard <114919150+damer-l@users.noreply.github.com>
2022-10-03 14:04:44 +0200

    Add files via upload

---
+++ boundaries-small_m-2.m
@@ -0,0 +1,53 @@
+{j[Base, 0, 0, 1, 0] -> m2*(-1 - ep^(-1) + Log[m2/mu2] + 
+    ep*(-1 - Pi^2/12 + Log[m2/mu2] - Log[m2/mu2]^2/2) + 
+    ep^2*(-1 - Pi^2/12 + (1 + Pi^2/12)*Log[m2/mu2] - Log[m2/mu2]^2/2 + 
+      Log[m2/mu2]^3/6 + Zeta[3]/3) + ep^3*(-1 - Pi^2/12 - Pi^4/160 + 
+      (-1/2 - Pi^2/24)*Log[m2/mu2]^2 + Log[m2/mu2]^3/6 - Log[m2/mu2]^4/24 + 
+      Log[m2/mu2]*(1 + Pi^2/12 - Zeta[3]/3) + Zeta[3]/3) + 
+    ep^4*(-1 - Pi^2/12 - Pi^4/160 + (1/6 + Pi^2/72)*Log[m2/mu2]^3 - 
+      Log[m2/mu2]^4/24 + Log[m2/mu2]^5/120 + Log[m2/mu2]*
+       (1 + Pi^2/12 + Pi^4/160 - Zeta[3]/3) + Log[m2/mu2]^2*
+       (-1/2 - Pi^2/24 + Zeta[3]/6) + Zeta[3]/3 + (Pi^2*Zeta[3])/36 + 
+      Zeta[5]/5) + ep^5*(-1 - Pi^2/12 - Pi^4/160 - (61*Pi^6)/120960 + 
+      (-1/24 - Pi^2/288)*Log[m2/mu2]^4 + Log[m2/mu2]^5/120 - 
+      Log[m2/mu2]^6/720 + Log[m2/mu2]^3*(1/6 + Pi^2/72 - Zeta[3]/18) + 
+      Log[m2/mu2]^2*(-1/2 - Pi^2/24 - Pi^4/320 + Zeta[3]/6) + Zeta[3]/3 + 
+      (Pi^2*Zeta[3])/36 - Zeta[3]^2/18 + Log[m2/mu2]*
+       (1 + Pi^2/12 + Pi^4/160 - Zeta[3]/3 - (Pi^2*Zeta[3])/36 - Zeta[5]/5) + 
+      Zeta[5]/5)), j[Base, 0, 1, 0, 1] -> 2 + ep^(-1) - Log[-(u/mu2)] + 
+   ep*(4 - Pi^2/12 - 2*Log[-(u/mu2)] + Log[-(u/mu2)]^2/2) + 
+   ep^2*(8 - Pi^2/6 + (-4 + Pi^2/12)*Log[-(u/mu2)] + Log[-(u/mu2)]^2 - 
+     Log[-(u/mu2)]^3/6 - (7*Zeta[3])/3), j[Base, 1, 0, 1, 0] -> 
+  2 + ep^(-1) - Log[-(s/mu2)] + ep*(4 - Pi^2/12 - 2*Log[-(s/mu2)] + 
+     Log[-(s/mu2)]^2/2) + ep^2*(8 - Pi^2/6 + (-4 + Pi^2/12)*Log[-(s/mu2)] + 
+     Log[-(s/mu2)]^2 - Log[-(s/mu2)]^3/6 - (7*Zeta[3])/3), 
+ j[Base, 0, 1, 1, 1] -> -Pi^2/(6*u) - Log[-(m^2/u)]^2/(2*u) + 
+   ep*((Pi^2*Log[-(m^2/u)])/(3*u) + Log[-(m^2/u)]^3/(3*u) + 
+     (Pi^2*Log[-(u/mu2)])/(6*u) + (Log[-(m^2/u)]^2*Log[-(u/mu2)])/(2*u) + 
+     (2*Zeta[3])/u) + ep^2*(-Pi^4/(90*u) - Log[-(m^2/u)]^4/(8*u) - 
+     (Pi^2*Log[-(m^2/u)]*Log[-(u/mu2)])/(3*u) - 
+     (Log[-(m^2/u)]^3*Log[-(u/mu2)])/(3*u) - (Pi^2*Log[-(u/mu2)]^2)/(12*u) + 
+     Log[-(m^2/u)]^2*((-5*Pi^2)/(24*u) - Log[-(u/mu2)]^2/(4*u)) - 
+     (2*Log[-(u/mu2)]*Zeta[3])/u), j[Base, 1, 0, 1, 1] -> 
+  -(Log[-(m^2/s)]^2/s) + ep*((Pi^2*Log[-(m^2/s)])/(3*s) + Log[-(m^2/s)]^3/s + 
+     (Log[-(m^2/s)]^2*Log[-(s/mu2)])/s + (2*Zeta[3])/s) + 
+   ep^2*(Pi^4/(180*s) - (7*Log[-(m^2/s)]^4)/(12*s) - 
+     (Pi^2*Log[-(m^2/s)]*Log[-(s/mu2)])/(3*s) - 
+     (Log[-(m^2/s)]^3*Log[-(s/mu2)])/s + Log[-(m^2/s)]^2*
+      ((-5*Pi^2)/(12*s) - Log[-(s/mu2)]^2/(2*s)) - (2*Log[-(s/mu2)]*Zeta[3])/
+      s), j[Base, 1, 1, 0, 1] -> -Pi^2/(6*u) - Log[-(m^2/u)]^2/(2*u) + 
+   ep*((Pi^2*Log[-(m^2/u)])/(3*u) + Log[-(m^2/u)]^3/(3*u) + 
+     (Pi^2*Log[-(u/mu2)])/(6*u) + (Log[-(m^2/u)]^2*Log[-(u/mu2)])/(2*u) + 
+     (2*Zeta[3])/u) + ep^2*(-Pi^4/(90*u) - Log[-(m^2/u)]^4/(8*u) - 
+     (Pi^2*Log[-(m^2/u)]*Log[-(u/mu2)])/(3*u) - 
+     (Log[-(m^2/u)]^3*Log[-(u/mu2)])/(3*u) - (Pi^2*Log[-(u/mu2)]^2)/(12*u) + 
+     Log[-(m^2/u)]^2*((-5*Pi^2)/(24*u) - Log[-(u/mu2)]^2/(4*u)) - 
+     (2*Log[-(u/mu2)]*Zeta[3])/u), j[Base, 1, 1, 1, 0] -> 
+  -Log[-(m^2/s)]^2/(2*s) + ep*((Pi^2*Log[-(m^2/s)])/(6*s) + 
+     Log[-(m^2/s)]^3/(3*s) + (Log[-(m^2/s)]^2*Log[-(s/mu2)])/(2*s) + 
+     (3*Zeta[3])/s) + ep^2*(Pi^4/(72*s) - Log[-(m^2/s)]^4/(8*s) - 
+     (Log[-(m^2/s)]^3*Log[-(s/mu2)])/(3*s) + Log[-(m^2/s)]^2*
+      (-Pi^2/(8*s) - Log[-(s/mu2)]^2/(4*s)) - (3*Log[-(s/mu2)]*Zeta[3])/s + 
+     Log[-(m^2/s)]*(-(Pi^2*Log[-(s/mu2)])/(6*s) - Zeta[3]/s)), 
+ j[Base, 1, 1, 1, 1] -> (-2*Pi^2)/(3*s*u) + (5*Log[-(m^2/u)]^2)/(2*s*u) - 
+   (3*Log[-(m^2/u)]*Log[s/u])/(s*u) + Log[s/u]^2/(2*s*u)}
